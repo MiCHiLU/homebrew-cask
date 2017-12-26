90ec38864d07a0c9a170d3646b2c3fed14286f2b
ENDOH takanao <djmchl@gmail.com>
2017-12-26 13:02:02 +0700

    add OLYMPUS A-GPS Utility 1.1.1

---
+++ Casks/olympus-a-gps-utility.rb
@@ -0,0 +1,10 @@
+cask 'olympus-a-gps-utility' do
+  version '1.1.1'
+  sha256 'e312e5ad99203915d57b0b3dc7f9fae9e1909cab269c56c0b1388265e6a38f27'
+
+  url 'http://sdl.olympus-imaging.com/agps/data/AGPSUtilSetup_v111.dmg'
+  name 'OLYMPUS A-GPS Utility'
+  homepage 'http://sdl.olympus-imaging.com/agps/index.en.html'
+
+  app 'OLYMPUS A-GPS Utility.app'
+end

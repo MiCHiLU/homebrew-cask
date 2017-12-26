3810bd44746e9d9251ee44a7fc67db20c9188bdd
ENDOH takanao <djmchl@gmail.com>
2017-12-26 15:13:02 +0700

    Add QTAmateur for MacOS X 1.1

---
+++ Casks/qtamateur.rb
@@ -0,0 +1,10 @@
+cask 'qtamateur' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.mikeash.com/software/qtamateur/QTAmateur.app.zip'
+  name 'QTAmateur for MacOS X'
+  homepage 'https://www.mikeash.com/software/qtamateur/'
+
+  app 'QTAmateur.app'
+end
